acb41911e9cd2d41d2cedb495702a34c14e6a276
karuga-ken <74537859+karuga-ken@users.noreply.github.com>
2024-04-23 13:08:20 +0300

    Created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignUp from "./pages/SignUp"
+import SignInn from "./pages/SignInn";
 
 
 function App() {
   return (
     <>
-      <h1> ken is a good dev!!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/sign-in" element={<SignInn/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
